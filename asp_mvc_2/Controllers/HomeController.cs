f510782fc935d1fd12c24177f9f425b41bd76ff3
faizaaulia <faizaaulia20@gmail.com>
2017-02-01 07:46:09 +0700

    Show List of Users in AdminOnly

--- asp_mvc_2/Controllers/HomeController.cs
+++ asp_mvc_2/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Web.Mvc;
 using asp_mvc_2.Security;
+using asp_mvc_2.Models.ViewModel;
+using asp_mvc_2.Models.EntityManager;
 
 namespace asp_mvc_2.Controllers
 {
@@ -23,6 +25,18 @@ namespace asp_mvc_2.Controllers
 
         public ActionResult UnAuthorized()
         {
+            return View();
+        }
+        [AuthorizeRoles("Admin")]
+        public ActionResult ManageUserPartial()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                string loginName = User.Identity.Name;
+                UserManager UM = new UserManager(); UserDataView UDV = UM.GetUserDataView(loginName);
+                return PartialView(UDV);
+            }
+
             return View();
         }
     }
